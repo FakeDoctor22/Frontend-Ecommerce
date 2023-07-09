4c01b98235795e380fb18abf83e8cf161f726213
FakeDoctor22 <giljoelvillarbaatwork@gmail.com>
2023-07-10 00:38:34 +0800

    add server

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:'/',
   plugins: [react()],
-  
+  server: {
+    proxy: "https://pawfectchewdeliveryapp.onrender.com", 
+  },
 })
